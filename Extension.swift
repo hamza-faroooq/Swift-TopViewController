ff7c64fb56c8caeca4d70202fca4181b6031ba38
Hamza Farooq <hamza_faroooq@yahoo.com>
2021-03-10 10:39:24 +0500

    Update Extension.swift

--- Extension.swift
+++ Extension.swift
@@ -5,17 +5,18 @@ extension UIApplication {
     class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
         
         if let navigationController = controller as? UINavigationController {
-            return navigationController.visibleViewController
+            return topViewController(controller: navigationController.visibleViewController)
         }
         
         if let tabController = controller as? UITabBarController {
             if let selected = tabController.selectedViewController {
-                return selected
+                return topViewController(controller: selected)
             }
         }
         
         if let presented = controller?.presentedViewController {
-            return presented
+            return topViewController(controller: presented)
+
         }
         
         return controller
